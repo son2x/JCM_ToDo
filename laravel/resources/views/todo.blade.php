d06be1f7c06d274e2fe91d3ed8ac4b4de93ea3c8
JCMillan <jcmillan@smart.com.ph>
2018-07-30 13:00:13 +0800

    updated todo.blade.php with table content

--- laravel/resources/views/todo.blade.php
+++ laravel/resources/views/todo.blade.php
@@ -13,8 +13,22 @@
 @section('todo-list')
     <div>
         <table>
-            <thead></thead>
-            <tbody></tbody>
+            <thead>
+                <tr>
+                    <th>Head 1</th>
+                    <th>Head 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Row 1 Cell 1</td>
+                    <td>Row 1 Cell 2</td>
+                </tr>
+                <tr>
+                    <td>Row 2 Cell 1</td>
+                    <td>Row 2 Cell 2</td>
+                </tr>
+            </tbody>
         </table>
     <div>
 @endsection
