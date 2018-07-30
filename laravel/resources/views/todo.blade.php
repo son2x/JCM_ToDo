5bcf8dca0c31b42d1f7fefa1b272c18feff625d6
JCMillan <jcmillan@smart.com.ph>
2018-07-30 13:23:01 +0800

    added 3rd row to table in todo.blade.php

--- laravel/resources/views/todo.blade.php
+++ laravel/resources/views/todo.blade.php
@@ -28,6 +28,10 @@
                     <td>Row 2 Cell 1</td>
                     <td>Row 2 Cell 2</td>
                 </tr>
+                <tr>
+                        <td>Row 3 Cell 1</td>
+                        <td>Row 3 Cell 2</td>
+                    </tr>
             </tbody>
         </table>
     <div>
